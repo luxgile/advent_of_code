7679940750aa4cb1a5ed59561f93bb514b29934e
luxgile <bisslit.12.2@gmail.com>
2023-12-02 16:19:05 +0100

    day 1 p1 completed, day 1 p2 wip

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -3,17 +3,47 @@ use std::str::FromStr;
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let result = 0;
+    let mut result = 0;
     for line in input.lines() {
-        let digits: String = line.chars().filter(|c| c.is_digit(10)).collect();
-        let res = digits.chars().nth(n) + digits.chars().last();
-        result += i32::from_str(res).unwrap();
+        let line_res = get_numbers_from_line(line);
+        result += u32::from_str(line_res.as_str()).unwrap()
     }
-    None
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut result = 0;
+    for line in input.lines() {
+        print!("{}", line);
+        let line = replace_numbers(line);
+        print!(" - {}", line);
+        let line_res = get_numbers_from_line(line.as_str());
+        print!(" - {}\n", line_res);
+        result += u32::from_str(line_res.as_str()).unwrap()
+    }
+    Some(result)
+}
+
+fn replace_numbers(line: &str) -> String {
+    let mut nline = String::from_str(line).unwrap();
+    nline = nline.replace("zero", "0");
+    nline = nline.replace("one", "1");
+    nline = nline.replace("two", "2");
+    nline = nline.replace("three", "3");
+    nline = nline.replace("four", "4");
+    nline = nline.replace("five", "5");
+    nline = nline.replace("six", "6");
+    nline = nline.replace("seven", "7");
+    nline = nline.replace("eight", "8");
+    nline = nline.replace("nine", "9");
+    nline
+}
+
+fn get_numbers_from_line(line: &str) -> String {
+    let mut digits = line.chars().filter(|c| c.is_digit(10));
+    let first = digits.clone().next().unwrap();
+    let last = digits.next_back().unwrap();
+    format!("{}{}", first, last)
 }
 
 #[cfg(test)]
